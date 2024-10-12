3ae24293769653bc3d5bc89a5060eb45f3802135
Nelson Lojo <nelson.lojo@berkeley.edu>
2024-10-12 23:05:21 +0000

    continuing refactoring 2 of 3

--- src/rsallms/game.py
+++ src/rsallms/game.py
@@ -1,6 +1,6 @@
 
 import random
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 import requests
 import json
 
@@ -24,69 +24,102 @@ class Category:
     def matches(self, words: list[str]) -> bool:
         return set(words) == set(self.members)
 
+    def diff(self, other_category: "Category") -> int:
+        """
+        Get the number of words mismatching between two categories
+        """
+        this_set = set(self.members)
+        other_set = set(other_category.members)
+        return len(this_set.symmetric_difference(other_set))
+
 
 class Connections:
+    """
+    A single game of Connections
+    """
 
     @property
     def all_words(self) -> list[str]:
         return [
             word
-            for group in self.groups
+            for group in self.categories
             for word in group.members
         ]
 
-    # @property
-    # def get_words_per_group(self) -> list[str]:
-
-    #     return [f"Category(level={group.level}, group='{group.group}', members={group.members})" for group in self.groups]
+    @property
+    def get_words_per_group(self) -> list[dict[str, int | str | list[str]]]:
+        """
+        The categories in this game as a list of dictionaries
+        """
+        return [asdict(group) for group in self.categories]
 
     @property
-    def get_words_per_group(self) -> list[str]:
-        # Group categories in chunks of 4
-        grouped_categories = [
-            self.groups[i:i + 4] for i in range(0, len(self.groups), 4)
-        ]
-
-        # Format each group into the desired string representation
-        return [
-            [
-                {
-                    "level": group.level,
-                    "group": group.group,
-                    "members": group.members
-                }
-                for group in group_list
-            ]
-            for group_list in grouped_categories
-        ]
-
-    def __init__(self, groups: list[Category], group_size: int = 4, max_strikes: int = 9999):
+    def is_solved(self) -> bool:
         """
-        Initialize a Connections object with a list of groups and their associated members.
+        A game is solved if all categories have been guessed
+        """
+        return len(self.categories) == 0
+
+    @property
+    def is_over(self) -> bool:
+        """
+        A game is over if the max number of strikes is reached or
+        if the game is solved
+        """
+        return (
+            self.current_strikes >= self._max_strikes
+            or self.is_solved
+        )
+
+    @property
+    def solved_categories(self) -> list[bool]:
+        """
+        Produce a list of flags indicating which categories have
+        been guessed
+        """
+        return [
+            not (cat in self.categories)
+            for cat in self._og_groups
+        ]
+
+    def __init__(self, categories: list[Category], group_size: int = 4, max_strikes: int = 9999, starting_strikes: int = 0):
+        """
+        Initialize a Connections object with a list of categories and
+        their associated members.
 
         :param groups: a list of Category objects
         :param group_size: the size of each group (default: 4)
 
-        :raises ValueError: if not all groups have the size specified by group_size
+        :raises ValueError: if not all groups have the size specified
+        by group_size
         """
         # Check that all groups have the same size
-        if not all(len(group.members) == group_size for group in groups):
-            raise ValueError(f"All groups must have exactly {
-                             group_size} members")
+        if not all(len(group.members) == group_size for group in categories):
+            raise ValueError(f"All groups must have exactly 
+                             {group_size} members")
 
         self._max_strikes = max_strikes
-        self._og_groups = groups
-        self.groups = groups
-        self.current_strikes = 0
+        self._og_groups = categories
+        self.group_size = group_size
+        self.categories = categories
+        self.current_strikes = starting_strikes
 
     def get_groups_by_level(self, level: int) -> list[Category]:
         """Filter the groups in this game by their level"""
-        return [group for group in self.groups if group.level == level]
+        return [group for group in self.categories if group.level == level]
+    
+    def json(self) -> dict[str, list[dict[str, int | str | list[str]]] | int]:
+        return {
+            "groups": [asdict(g) for g in self.categories],
+            "group_size": len(self.categories[0].members),
+            "max_strikes": self._max_strikes,
+            "starting_strikes": self.current_strikes
+        }
 
     def guess(self, words: list[str]) -> Category | None:
         """
-        Returns the category associated with the given words if they match any of the groups
-        (and removes that category from the list), otherwise returns None.
+        Return the category associated with the guessed words if they match a category
+        (and removes that category from the game), otherwise return None and add a strike
         """
 
         if self.current_strikes >= self._max_strikes:
@@ -94,7 +127,7 @@ class Connections:
                 "Game over. You've reached the max number of strikes!")
 
         matches = [
-            group.matches(words) for group in self.groups
+            group.matches(words) for group in self.categories
         ]
 
         good_guess = any(matches)
@@ -104,10 +137,13 @@ class Connections:
             return None
 
         matched_group = matches.index(True)
-        return self.groups.pop(matched_group)
+        return self.categories.pop(matched_group)
 
     def reset(self):
-        self.groups = self._og_groups
+        """
+        Reset the game to its initial state
+        """
+        self.categories = self._og_groups
         self.current_strikes = 0
 
 
@@ -168,36 +204,49 @@ def load_daily_board() -> Connections:
     ])
 
 
-def load_games() -> list[dict]:
+def load_games() -> list[Connections]:
     """Load all games from the remote endpoint."""
     resp = requests.get(GAME_DATA_ENDPOINT)
     if resp.status_code != 200:
         raise Exception(f"Failed to get connections data: {resp.status_code}")
-    return resp.json()
+    
+    raw_data = resp.json()
+
+    if not isinstance(raw_data, list):
+        raise ValueError(f"Games data is not a list of games!")
+
+    return [
+        Connections(categories=[
+            Category(**cat)
+            for cat in game["answers"]
+        ]) for game in raw_data
+    ]
 
 
-def save_specific_game_indices_to_json(indices: list[int], filename='connections.json'):
+def save_specific_game_indices_to_json(indices: list[int], filename='connections.json') -> None:
     """Save the specified game connections to a JSON file."""
-    games = load_games()
-    categories = []
+    games: list[Connections] = load_games()
+    categories: list[Category] = [ ]
 
     for idx in indices:
         if idx < len(games):
-            sampled_game = games[idx]
+            game: Connections = games[idx]
             # Collect categories from this game
-            categories.extend(sampled_game["answers"])
+            categories.extend(game.categories)
+        else:
+            raise IndexError(f"Index {idx} is out of range!")
 
     # Save the categories to JSON
     with open(filename, 'w') as f:
         json.dump(categories, f)
 
-
-def load_json_to_connections(filename: str) -> Connections:
-    """Load categories from a JSON file into a Connections object."""
+def load_json_to_connections(filename: str) -> list[Connections]:
+    """Load list of categories from a JSON file into list of Connections games."""
     with open(filename, 'r') as f:
         data = json.load(f)
+
     categories = [Category(**item) for item in data]
-    return Connections(categories)
+    return [ Connections(categories[i:+4]) for i in range(0, len(categories), 4) ]
 
 
 if __name__ == "__main__":
@@ -218,4 +267,4 @@ if __name__ == "__main__":
 
     # Load the connections from the saved JSON file
     test_connections = load_json_to_connections('test_connections.json')
-    print(test_connections.get_words_per_group[12])
+    print(test_connections[12].get_words_per_group)
